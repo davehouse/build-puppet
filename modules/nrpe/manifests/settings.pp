0d97185f7dc430cfd4ded431e9894c326adb232c
davehouse <dhouse@mozilla.com>
2019-10-31 07:45:11 -0600

    nrpe module comparison

--- modules/nrpe/manifests/settings.pp
+++ modules/nrpe/manifests/settings.pp
@@ -1,19 +1,17 @@
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
-class nrpe::settings {
-    include ::shared
 
-    case $::operatingsystem {
-        Darwin: {
+class nrpe::settings {
+
+    $nrpe_etcdir = '/etc/nagios/'
+
+    case $facts['os']['name'] {
+        'Darwin': {
             $plugins_dir = '/usr/local/libexec'
         }
-        Ubuntu: {
-            $plugins_dir = '/usr/lib/nagios/plugins'
-        }
         default: {
-            $plugins_dir = "/usr/${::shared::lib_arch_dir}/nagios/plugins"
+            fail("${$facts['os']['name']} not suported")
         }
     }
-    $nrpe_etcdir = '/etc/nagios/'
 }
